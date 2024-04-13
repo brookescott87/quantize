a83a17afa162b5cd555be4497eaf05bc2019b78a
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2024-04-13 13:30:42 -0400

    Added model_name.py

---
+++ model_name.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+toastlib = str(Path('C:/Apps/Toaster/lib/python'))
+
+if toastlib not in sys.path:
+    sys.path.insert(0, toastlib)
+
+from gguf import GGUFReader
+
+def model_name(model):
+    try:
+        gguf = GGUFReader(model, 'r')
+        return str(bytes(gguf.fields['general.name'].parts[-1]), encoding='utf-8')
+    except:
+        return '?'
+
+models = sys.argv[1:]
+for m in models:
+    print('%s: "%s"'%(m, model_name(m)))
