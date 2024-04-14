3277ffed1356e8c9f0cc1e209bd7143d11ea2e88
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2024-04-14 18:18:17 -0400

    Reworked logic of how files are selected for upload

--- upload.py
+++ upload.py
@@ -13,6 +13,8 @@ split_rx = re.compile('.*-split-\d{5}-of-\d{5}\.gguf$')
 TOASTER = Path(os.environ['TOASTER'])
 gguf_split_exe = TOASTER/'bin'/'gguf-split'
 
+removed = set()
+
 def timestamp():
     return dt.strftime(dt.now(get_localzone()), '%Y/%m/%d-%H:%M:%S(%Z)')
 
@@ -25,6 +27,14 @@ def print_object(p, obj):
     else:
         print(f'Wrote {type(obj).__name__} to {p}')
 
+def next_file(dirp):
+    for g in ('*.imatrix','*.gguf'):
+        for f in dirp.glob(g):
+            if f not in removed:
+                if f.is_file() and not f.is_symlink():
+                    return f
+    return None
+
 def gguf_split(p: Path):
     result = subprocess.run([gguf_split_exe, '--split-max-size', str(MAX_UPLOAD_SIZE), p])
     if result.returncode:
@@ -35,11 +45,15 @@ if (hf_key := 'HF_HUB_ENABLE_HF_TRANSFER') in os.environ:
     del os.environ[hf_key]
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--all', '-a', action='store_true',
-                    help='All images, even splits')
+parser.add_argument('--dir', '-d', type=Path,
+                    help='Directory for source files')
+parser.add_argument
 args = parser.parse_args()
 
 hfapi = huggingface_hub.HfApi()
+
+if args.dir:
+    os.chdir(args.dir)
 
 cwd = Path('.')
 acwd = cwd.absolute()
@@ -47,27 +61,21 @@ repo = acwd.name
 owner = acwd.parent.name
 repo_id = f'{owner}/{repo}'
 
-while True:
-    active = False
-    for f in cwd.glob('*.gguf'):
-        if f.is_file() and not f.is_symlink():
-            if args.all or not split_rx.match(f.name):
-                if f.stat().st_size <= MAX_UPLOAD_SIZE:
-                    try:
-                        if hfapi.file_exists(repo_id, f.name):
-                            print(f'Removing {f.name}')
-                            f.unlink()
-                        else:
-                            active = True
-                            print(f'Uploading {f.name} to {repo_id}/{f.name}')
-                            v = hfapi.upload_file(path_or_fileobj=f, repo_id=repo_id, path_in_repo=f.name,
-                                                commit_message=f'Upload {f.name}')
-                            print(f'{f.name} succeeded')
-                            print_object(f.with_suffix('.log'), v)
-                    except Exception as ex:
-                        print(f'\n{f.name} failed due to {type(ex).__name__}')
-                        print_object(f.with_suffix('.err'), ex)
-    if active:
-        print("Re-looping...")
+while f := next_file(cwd):
+    if f.stat().st_size > MAX_UPLOAD_SIZE:
+        gguf_split(f)
     else:
-        break
+        try:
+            if hfapi.file_exists(repo_id, f.name):
+                print(f'Removing {f.name}')
+                removed.add(f)
+                f.unlink()
+            else:
+                print(f'Uploading {f.name} to {repo_id}/{f.name}')
+                v = hfapi.upload_file(path_or_fileobj=f, repo_id=repo_id, path_in_repo=f.name,
+                                    commit_message=f'Upload {f.name}')
+                print(f'{f.name} succeeded')
+                print_object(f.with_suffix('.log'), v)
+        except Exception as ex:
+            print(f'\n{f.name} failed due to {type(ex).__name__}')
+            print_object(f.with_suffix('.err'), ex)
