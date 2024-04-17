5783023457e69227cab048fe73fa87c0e2623c45
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2024-04-17 15:50:03 -0400

    Just print ??? if there's a problem.

--- imatrix_model.py
+++ imatrix_model.py
@@ -10,11 +10,15 @@ try:
 except:
     model_list = []
 
-for model in model_list:
-    fn = os.path.normpath(model)
-    size = abs(target - os.path.getsize(fn))
-    if not winner_name or size < winner_size:
-        winner_name = model
-        winner_size = size
+try:
+    for model in model_list:
+        fn = os.path.normpath(model)
+        size = abs(target - os.path.getsize(fn))
+        if not winner_name or size < winner_size:
+            winner_name = model
+            winner_size = size
 
-print(winner_name)
+    print(winner_name)
+
+except Exception:
+    print('???')
