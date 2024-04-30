8bd5440d8023e045fcb283467842bb40b1be76c2
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2024-04-29 22:56:08 -0400

    Using convert-hf-to-gguf for conversions

--- quantize.mk
+++ quantize.mk
@@ -31,7 +31,8 @@ xquantize = \
 quantize = \
 	$(call xquantize,$3,$(call qtype,$3),$(filter %.gguf,$2),$(filter %.imatrix,$2)) && $(call install,$3,$1-GGUF,$4)
 
-convert := python $(LLAMA_CPP_BIN)/convert.py --pad-vocab ${convert_opts}
+#convert := python $(LLAMA_CPP_BIN)/convert.py --pad-vocab ${convert_opts}
+convert := python $(LLAMA_CPP_BIN)/convert-hf-to-gguf.py
 imatrix := $(LLAMA_CPP_BIN)/imatrix.exe -f $(LLAMA_CPP_DATA)/20k_random_data.txt $(IMATRIX_OPTS)
 imatrix_model := python imatrix_model.py
 
