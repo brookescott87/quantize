2164689fba3958aa9a00822da09bcd364143c552
Brooke <brooketh@faraday>
2024-05-01 18:07:00 +0000

    Added shebang paths

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 from pathlib import Path
 import huggingface_hub
--- mkreadme.py
+++ mkreadme.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 from string import Template
 from pathlib import Path, PurePosixPath as RepoPath
