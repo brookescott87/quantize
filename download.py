15afb056304d60adfe6ed99c610d6f2651989da2
Brooke <brooketh@faraday>
2024-05-12 22:45:52 -0400

    added --no-symlink option to download.py

--- download.py
+++ download.py
@@ -14,6 +14,8 @@ def main():
                         help='Directory into which a link to the model will be put')
     parser.add_argument('--affix', '-a', type=str, default='',
                         help='Local affix to model name')
+    parser.add_argument('--no-symlink', '-u', action='store_true',
+                        help='Do not symlink to model path')
     args = parser.parse_args()
 
     if args.affix and not args.affix.startswith('-'):
@@ -23,22 +25,30 @@ def main():
         raise ValueError('repo_id must be of the form owner/model')
     owner, model = args.repo_id.split('/')
 
-    if not args.destdir:
-        if model.upper().endswith('-GGUF'):
-            args.destdir = Path('models/ref')
-            model = model[:-5] + args.affix + '-GGUF'
-        else:
-            args.destdir = Path('models/base')
-            model = model + args.affix
+    if args.no_symlink:
+        if args.destdir:
+            print('warning: destdir is ignored when --no-symlink given')
+            args.destdir = None
+    else:
+        if not args.destdir:
+            if model.upper().endswith('-GGUF'):
+                args.destdir = Path('models/ref')
+                model = model[:-5] + args.affix + '-GGUF'
+            else:
+                args.destdir = Path('models/base')
+                model = model + args.affix
 
-    if not args.destdir.is_dir():
-        if args.destdir.exists():
-            raise ValueError('%s is not a directory'%(args.destdir,))
-        args.destdir.mkdir(parents=True)
-    model_path = args.destdir / model
+        if not args.destdir.is_dir():
+            if args.destdir.exists():
+                raise ValueError('%s is not a directory'%(args.destdir,))
+            args.destdir.mkdir(parents=True)
+        model_path = args.destdir / model
 
     cache_path = Path(hfapi.snapshot_download(repo_id=args.repo_id))
-    model_path.symlink_to(cache_path, True)
+    if args.no_symlink:
+        model_path = cache_path
+    else:
+        model_path.symlink_to(cache_path, True)
     print(f'\n\n\n\n\n\nModel downloaded to: {model_path.absolute()}')
 
 if __name__ == '__main__':
