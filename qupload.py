8eb670b9cfca86517ac57bc66920008b3b72dc33
Brooke <brooketh@faraday>
2024-05-19 06:36:00 +0000

    major refactor

--- qupload.py
+++ qupload.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 import os
+import re
 from pathlib import Path
 from datetime import datetime as dt
+from typing import Iterator,List
 import subprocess
 import clear_screen
 import huggingface_hub
@@ -11,11 +14,54 @@ import argparse
 MAX_UPLOAD_SIZE = 50_000_000_000
 TOASTER = Path(os.environ['TOASTER_ROOT'])
 gguf_split_exe = TOASTER/'bin'/'gguf-split'
+shard_rx = re.compile('.*-split-\d{5}-of-\d{5}$')
 
 hfapi = huggingface_hub.HfApi()
 
-def oversize_ggufs(d: Path) -> list[Path]:
-    return [f for f in d.iterdir() if f.suffix == '.gguf' and f.stat().st_size > MAX_UPLOAD_SIZE]
+class Uploader(object):
+    def __init__(self, repo_id: str, folder_path: Path, max_retries:int = 0):
+        self.repo_id = repo_id
+        self.folder_path = folder_path
+        self.max_retries = max_retries
+        self.total_retries = 0
+        self.start_time = dt.now()
+
+    @property
+    def elapsed(self):
+        return dt.now() - self.start_time
+        
+    def upload(self, message: str, allow_patterns:List[str], ignore_patterns:List[str], skip=False):
+        if skip:
+            return True
+        retries = 0
+        finished = False
+
+        while not (finished or (self.max_retries and retries > self.max_retries)):
+            clear_screen.clear()
+            sys.stdout.write(message)
+
+            if retries:
+                sys.stdout.write(f' (retry {retries}')
+                if self.max_retries:
+                    sys.stdout.write(f' of {self.max_retries}')
+                    sys.stdout.write(')')
+            sys.stdout.write('\n')
+            try:
+                hfapi.upload_folder(repo_id=self.repo_id, folder_path=self.folder_path, commit_message=message,
+                                    repo_type='model', allow_patterns=allow_patterns, ignore_patterns=ignore_patterns)
+                finished = True
+            except KeyboardInterrupt:
+                print('\n*** Keyboard interrupt ***')
+                break
+            except RuntimeError:
+                retries += 1
+                print('Upload failed')
+
+        self.total_retries += retries
+        return finished
+
+def oversize_ggufs(d: Path) -> Iterator[Path]:
+    return (f for f in d.iterdir() if f.suffix == '.gguf' and f.stat().st_size > MAX_UPLOAD_SIZE)
 
 def gguf_split(p: Path, keep=False):
     result = subprocess.run([gguf_split_exe, '--split-max-size', str(MAX_UPLOAD_SIZE), p])
@@ -27,10 +73,12 @@ def gguf_split(p: Path, keep=False):
         p.unlink()
 
 def check_quant(qdir, keep_oversize=False):
-    qbig = oversize_ggufs(qdir)
+    qbig = list(oversize_ggufs(qdir))
     for f in qbig:
+        if shard_rx.match(f.stem):
+            raise RuntimeError(f'GGUF shard "{f}" is oversize!')
         gguf_split(f, keep_oversize)
-    if oversize_ggufs(qdir):
+    if any(oversize_ggufs(qdir)):
         raise RuntimeError(f'"{qdir}" still has oversize ggufs')
 
 def main():
@@ -38,24 +86,31 @@ def main():
     parser.add_argument('directory', type=Path, help='Directory containing quant.')
     parser.add_argument('--initialize', '-i', action='store_true', help="Create new if doesn't exist") 
     parser.add_argument('--retries', '-r', type=int, default=0, help='Number of times to retry')
+    parser.add_argument('--only_shards', '-S', action='store_true', help='Only shards')
     parser.add_argument('--no-ggufs', '-g', action='store_true', help='Exclude GGUFs from upload')
     parser.add_argument('--no-upload', '-u', action='store_true', help='Do not upload any files')
     parser.add_argument('--keep-oversize', '--keep', '-k', action='store_true', help='Keep oversize GGUFs after splitting')
     args = parser.parse_args()
 
-    upload_patterns = [
+    allow_patterns = [
         'README.md',
         '*.png',
         '*.imatrix'
     ]
 
+    gguf_pattern = '*.gguf'
+    shard_pattern = '*-split-?????-of-?????.gguf'
+
+    ignore_patterns = []
+    
     qdir = args.directory.absolute()
 
     if not qdir.is_dir():
         raise ValueError(f'"{qdir}" is not a directory.')
 
     if not args.no_ggufs:
-        upload_patterns.append('*.gguf')
+        allow_patterns.append(gguf_pattern)
+        ignore_patterns.append(shard_pattern)
         check_quant(qdir, args.keep_oversize)
 
     repo = qdir.name
@@ -70,25 +125,18 @@ def main():
             raise ValueError(f'Repository {repo_id} does not exist and --initialize not given')
 
     if not args.no_upload:
-        print(f'Uploading {repo_id}')
+        uploader = Uploader(repo_id, qdir, args.retries)
 
-        running = True
-        retries = 0
-        start_time = dt.now()
+        if (success := uploader.upload(f'Upload {repo}', allow_patterns, ignore_patterns, args.only_shards)):
+            if not args.no_ggufs:
+                if any(qdir.glob(shard_pattern)):
+                    success = uploader.upload('Upload shards', [shard_pattern], [])
 
-        while running and retries <= args.retries:
-            try:
-                hfapi.upload_folder(repo_id=repo_id, folder_path=qdir, commit_message=f'Upload {repo}.',
-                                    repo_type='model', allow_patterns=upload_patterns)
-                running = False
-            except RuntimeError:
-                retries += 1
-                print(f'Upload failed (retries = {retries})')
-
-        elapsed = dt.now() - start_time
-        result = 'failed' if running else 'succeeded'
         clear_screen.clear()
-        print(f"# Upload {result} after {elapsed} and {retries} retr{'y' if retries == 1 else 'ies'}.")
+        print('# Upload %s after %s and %d retr%s.'%('succeeded' if success else 'failed',
+                                                     uploader.elapsed,
+                                                     retries := uploader.total_retries,
+                                                     'y' if retries == 1 else 'ies'))
 
 if __name__ == '__main__':
     main()
