bca868777a12a915d530ddab52f26a7347e3dbd3
Brooke <brooketh@faraday>
2024-05-30 10:01:47 -0400

    added varname to misc.py

--- qlib/misc.py
+++ qlib/misc.py
@@ -57,3 +57,6 @@ class ProxyObject:
 def to_json(obj:Any, readable=False):
     dump_opts = {'indent': 4} if readable else { 'separators': (',',':') }
     return json.dumps(obj, ensure_ascii=False, **dump_opts)
+
+def varname(s:str):
+    return s.lower().replace(' ','_')
