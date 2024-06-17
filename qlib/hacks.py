90f499e6f39fe8aed8f1c92798c91df8f438baaf
Brooke <brooketh@faraday>
2024-06-17 15:11:34 -0400

    include type information in TypeError

--- qlib/hacks.py
+++ qlib/hacks.py
@@ -4,7 +4,7 @@ def path_add_str(self: Path, other) -> Path:
     if isinstance(other, str):
         return self.with_name(self.name + other)
     else:
-        raise TypeError('Unsupported operand type(s) for +')
+        raise TypeError(f'Unsupported operand type(s) for +: {repr(type(self))} and {repr(type(other))}')
 
 if not hasattr(Path, '__add__'):
     setattr(Path, '__add__', path_add_str)
