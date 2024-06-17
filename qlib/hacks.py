2fae5df0f0ce2b0c1638c15c7f3f130833709dee
Brooke <brooketh@faraday>
2024-06-17 15:12:34 -0400

    Added initattr function

--- qlib/hacks.py
+++ qlib/hacks.py
@@ -1,17 +1,14 @@
 from pathlib import Path
 
+def initattr(obj, name, value):
+    if not hasattr(obj, name):
+        setattr(obj, name, value)
+
 def path_add_str(self: Path, other) -> Path:
     if isinstance(other, str):
         return self.with_name(self.name + other)
     else:
         raise TypeError(f'Unsupported operand type(s) for +: {repr(type(self))} and {repr(type(other))}')
+initattr(Path, '__add__', path_add_str)
 
-if not hasattr(Path, '__add__'):
-    setattr(Path, '__add__', path_add_str)
-
-@property
-def path_size(self: Path):
-    return self.stat().st_size
-
-if not hasattr(Path, 'size'):
-    setattr(Path, 'size', path_size)
+initattr(Path, 'size', property(lambda self: self.stat().st_size))
