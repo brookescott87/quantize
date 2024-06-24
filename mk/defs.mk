343fbff1809cd1dc966d06303ce46dfde4ef305f
Brooke <brooketh@faraday>
2024-06-24 15:31:55 -0400

    Added a postquantize step to the quantization process

--- mk/defs.mk
+++ mk/defs.mk
@@ -54,14 +54,14 @@ mkreadme_opts += $(if $(FULLNAME),--title $(FULLNAME))
 IQTYPES := IQ1_S IQ1_M IQ2_XXS IQ2_XS IQ2_S IQ2_M IQ3_XXS IQ3_XS IQ3_S IQ3_M IQ4_XS
 KQTYPES := Q3_K_S Q3_K_M Q3_K_L Q4_K_S Q4_K_M Q5_K_S Q5_K_M Q6_K Q8_0
 
-qtype = $(patsubst $(QUANTMODEL).%.gguf,%,$1)
+qtype = $(patsubst $(QUANTMODEL).%.xguf,%,$1)
 
 FTYPE := $(or $(FTYPE),$(default_ftype))
 
-IQUANTS := $(patsubst %,$(QUANTMODEL).%.gguf,$(IQTYPES))
-KQUANTS := $(patsubst %,$(QUANTMODEL).%.gguf,$(KQTYPES))
+IQUANTS := $(patsubst %,$(QUANTMODEL).%.xguf,$(IQTYPES))
+KQUANTS := $(patsubst %,$(QUANTMODEL).%.xguf,$(KQTYPES))
 QUANTS := $(IQUANTS) $(KQUANTS)
-PPLOUT := $(patsubst %.gguf,%.ppl.out,$(QUANTS))
+PPLOUT := $(patsubst %.xguf,%.ppl.out,$(QUANTS))
 ASSETS := $(notdir $(wildcard $(ASSETDIR)/*.png))
 
 convert_py := convert-hf-to-gguf.py $(if $(PRETOKENIZER),--vocab-pre=$(PRETOKENIZER))
@@ -72,7 +72,8 @@ imatrix_input := imatrix_dataset.txt
 imatrix = $(TOASTER_BIN)/imatrix $(IMATRIX_OPTS) -c 128 -m $1 $(ngl) -f $(imatrix_input) -o $2.tmp && mv $2.tmp $2
 mkreadme := python $(SCRIPTDIR)/mkreadme.py
 qupload := python $(SCRIPTDIR)/qupload.py
-quantize = $(TOASTER_BIN)/quantize $1 $2 $(call qtype,$2)
+postquantize := python $(SCRIPTDIR)/postquantize.py
+quantize = $(TOASTER_BIN)/quantize $1 $2.tmp $(call qtype,$2) && $(postquantize) $2.tmp && mv $2.tmp $2
 perplexity := $(TOASTER_BIN)/perplexity
 
 B := $(source)/$(BASEMODEL)
@@ -123,11 +124,11 @@ README.md: _meta.json GNUmakefile
 	rm -f $@
 	$(mkreadme) -m $< $(mkreadme_opts) -o $@ $(BASEREPO)
 
-$(IQUANTS): %.gguf:
+$(IQUANTS): %.xguf:
 	$(call quantize,--imatrix $Q.imatrix $Q.bin,$@)
 
-$(KQUANTS): %.gguf:
+$(KQUANTS): %.xguf:
 	$(call quantize,$Q.bin,$@)
 
-%.ppl.out: %.gguf $Q.klb
+%.ppl.out: %.xguf $Q.klb
 	$(perplexity) -m $< $(ngl) --kl-divergence --kl-divergence-base $Q.klb | tee $@.tmp && mv -f $@.tmp $@
---
+++ postquantize.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import os
+from pathlib import Path
+from typing import Iterable
+import qlib
+import hashlib
+import subprocess
+import argparse
+
+class PurgeFailed(Exception):
+    def __init__(self, path: Path):
+        self.path = path
+
+def init_paths():
+    def chk(p:Path) -> Path:
+        if not p.exists():
+            raise FileNotFoundError("'{p}' not found")
+        return p
+
+    global toaster_root, gguf_split_exe
+    if toaster_root := os.getenv('TOASTER_ROOT'):
+        toaster_root = Path(toaster_root)
+    else:
+        raise RuntimeError('TOASTER_ROOT environment variable is not set')
+    bin = chk(toaster_root / 'bin')
+    gguf_split_exe = chk(bin / 'gguf-split')
+
+def hash_file(p: Path):
+    if p.exists() and p.is_file():
+        outp = p.with_name(p.name + '.sha256')
+        outp.unlink(missing_ok = True)
+        fsize = p.stat().st_size
+        with open(p, 'rb') as srcfile:
+            if fsize > qlib.MAX_BLOB_SIZE:
+                h = hashlib.file_digest(srcfile, 'sha256')
+                with outp.open('wt') as outfile:
+                    outfile.write(h.hexdigest())
+
+def gguf_split(xguf, outp):
+    result = subprocess.run([gguf_split_exe, '--split-max-size', '50G', str(xguf), str(outp)])
+    if result.returncode:
+        raise RuntimeError(f'gguf-split returned {result.returncode}')
+
+def split_or_link(xguf: Path):
+    if xguf.size > qlib.MAX_UPLOAD_SIZE:
+        outp = xguf.with_suffix('') + '-split'
+        gguf_split(xguf, outp)
+        for p in xguf.parent.glob(outp.name + '*.gguf'):
+            yield(p)
+    else:
+        outp = xguf.with_suffix('.gguf')
+        outp.hardlink_to(xguf)
+        yield(outp)
+
+def purge(p, srcp=None):
+    if srcp and p.is_newer_than(srcp):
+        raise PurgeFailed(p, srcp)
+    p.unlink(missing_ok = True)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('xguf', type=Path, help='Name of xguf file to operate on')
+    args = parser.parse_args()
+    init_paths()
+    dirp = (tmpp := args.xguf).parent
+    if (stem := tmpp.name.removesuffix('.xguf.tmp')) == tmpp.name:
+        raise ValueError(f"{tmpp} doesn't have .xguf.tmp suffix")
+    any(map(purge, dirp.glob(stem + '*.gguf'), tmpp))
+    any(map(purge, dirp.glob(stem + '*.gguf.sha256')))
+    for outp in split_or_link(tmpp):
+        hash_file(outp)
+
+def run_main():
+    try:
+        main()
+    except PurgeFailed as nfe:
+        print(f"{nfe.path} is up to date")
+
+if __name__ == '__main__':
+    run_main()
