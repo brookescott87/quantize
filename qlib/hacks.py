e4c5db9530c79efb0b947b5d1c540a8333b1b8a8
Brooke <brooketh@backyardai>
2024-12-08 15:50:15 -0500

    Avoid polluting namespace

--- qlib/hacks.py
+++ qlib/hacks.py
@@ -1,26 +1,26 @@
-from pathlib import Path
 from typing import Any
+import pathlib
 import math
 
 def initattr(obj, name, value):
     if not hasattr(obj, name):
         setattr(obj, name, value)
 
-def path_add_str(self: Path, other) -> Path:
+def path_add_str(self: pathlib.Path, other) -> pathlib.Path:
     if isinstance(other, str):
         return self.with_name(self.name + other)
     else:
         raise TypeError(f'Unsupported operand type(s) for +: {repr(type(self))} and {repr(type(other))}')
-initattr(Path, '__add__', path_add_str)
+initattr(pathlib.Path, '__add__', path_add_str)
 
-initattr(Path, 'size', property(lambda self: self.stat().st_size))
-# initattr(Path, 'mtime',
+initattr(pathlib.Path, 'size', property(lambda self: self.stat().st_size))
+# initattr(pathlib.Path, 'mtime',
 #          property(lambda self: self.lstat().st_mtime if self.exists(follow_symlinks=False) else None))
-initattr(Path, 'mtime',
+initattr(pathlib.Path, 'mtime',
          property(lambda self: self.lstat().st_mtime if self.exists() else None))
 # For the purpose of comparison, nonexistent file is treated as though it were infinitely old
-initattr(Path, 'is_newer_than', lambda self, other: (self.mtime or -math.inf) > (other.mtime or -math.inf))
-initattr(Path, 'is_older_than', lambda self, other: (self.mtime or -math.inf) < (other.mtime or -math.inf))
+initattr(pathlib.Path, 'is_newer_than', lambda self, other: (self.mtime or -math.inf) > (other.mtime or -math.inf))
+initattr(pathlib.Path, 'is_older_than', lambda self, other: (self.mtime or -math.inf) < (other.mtime or -math.inf))
 
 import numpy
 import gguf
